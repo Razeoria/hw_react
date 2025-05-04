20c54a2037822b916b774cd2aa6881d90ae5d8cf
Razeoria <latemaestro@gmail.com>
2025-05-05 01:23:34 +0200

    hw14

---
+++ hw15/src/components/App/App.jsx
@@ -0,0 +1,17 @@
+
+
+import Users from '../Users/Users';
+
+import '../../styles/style.css';
+
+function App() {
+  
+
+  return (
+    <>
+     <Users />
+    </>
+  )
+}
+
+export default App
---
+++ hw15/src/components/UserList/UserListItem/UserListItem.jsx
@@ -0,0 +1,13 @@
+
+
+import styles from './UserListItem.module.css';
+
+const UserListItem = ({ item }) => {
+    return (
+        <li className={styles.item}>
+            {item.name}
+        </li>
+    )
+};
+
+export default UserListItem;
