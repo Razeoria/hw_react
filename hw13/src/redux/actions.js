715dedb7e2f2e1b7bc81fa0169e08a52e4a964fc
Razeoria-FE | Website Developer <Latemaestro@gmail.com>
2026-01-09 19:09:05 +0100

    Delete hw13 directory

--- hw13/src/redux/actions.js
+++
@@ -1,6 +0,0 @@
-import { SET_FILTER } from "./types";
-
-export const setFilter = filter => ({
-    type: SET_FILTER,
-    payload: filter,
-});
--- hw13/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import rootReducer from "./reducer";
-
-const appStore = legacy_createStore(rootReducer, devToolsEnhancer());
-
-export default appStore;
